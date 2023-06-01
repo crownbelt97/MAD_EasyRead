e0946a7e84be9ddeded31f3b55e652311f7a1e34
crownbelt97 <blaziken_jeremychan@live.com.sg>
2023-06-01 10:34:37 +0800

    No Action bar

--- app/src/main/java/sg/edu/np/mad/mad_easyread/LoginActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_easyread/LoginActivity.java
@@ -3,12 +3,17 @@ package sg.edu.np.mad.mad_easyread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
     }
 }
