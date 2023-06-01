b2d385e05d07b097ac8a93b74b1280c0a013bf53
crownbelt97 <blaziken_jeremychan@live.com.sg>
2023-06-01 10:07:42 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/np/mad/mad_easyread/LoginActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.mad_easyread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
