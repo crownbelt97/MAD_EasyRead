5c9a302210f052802adaf3833e4653c69bb048e9
crownbelt97 <blaziken_jeremychan@live.com.sg>
2023-06-17 15:03:19 +0800

    Added horizontal scroll view page in homepage

--- app/src/main/java/sg/edu/np/mad/mad_easyread/HomeFragment.java
+++ app/src/main/java/sg/edu/np/mad/mad_easyread/HomeFragment.java
@@ -20,8 +20,6 @@ import java.util.List;
 
 
 public class HomeFragment extends Fragment {
-    private ViewPager2 viewPager2;
-    private Handler sliderHandler = new Handler();
     public HomeFragment()
     {
 
@@ -30,58 +28,12 @@ public class HomeFragment extends Fragment {
     @Override
      public void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
-//          requestWindowFeature(Window.FEATURE_NO_TITLE);
-//          this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//          getSupportActionBar().hide();
-         //setContentView(R.layout.fragment_home);
-          //viewPager2 = viewPager2.findViewById(R.id.viewPagerImageSlider);
-
-                 List<SliderItem> sliderItems = new ArrayList<>();
-                 sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-                 sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-                 sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-                 sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-
-
      }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-//         viewPager2 = viewPager2.findViewById(R.id.viewPagerImageSlider);
-//
-//         List<SliderItem> sliderItems = new ArrayList<>();
-//         sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-//         sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-//         sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-//         sliderItems.add(new SliderItem(R.drawable.baseline_android_24));
-//
-//         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
-//
-//         viewPager2.setClipToPadding(false);
-//         viewPager2.setClipChildren(false);
-//         viewPager2.setOffscreenPageLimit(3);
-//
-//         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2));
-//
-//         CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-//         compositePageTransformer.addTransformer(new MarginPageTransformer(40));
-//
-//         compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
-//             @Override
-//             public void transformPage(@NonNull View page, float position) {
-//                 float r = 1 - Math.abs(position);
-//                 page.setScaleY(0.85f + r * 0.15f);
-//             }
-//         });
-//
-//
-//         viewPager2.setPageTransformer(compositePageTransformer);
-//         viewPager2.unregisterOnPageChangeCallback(new);
-
 
         return inflater.inflate(R.layout.fragment_home, container, false);
 
