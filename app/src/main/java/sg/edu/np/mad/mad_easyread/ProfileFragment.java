43770d453f64d56d1e16f46af30f16517041b7d4
KahYengP <s10245147@connect.np.edu.sg>
2023-06-17 17:19:34 +0800

    add profile details

--- app/src/main/java/sg/edu/np/mad/mad_easyread/ProfileFragment.java
+++ app/src/main/java/sg/edu/np/mad/mad_easyread/ProfileFragment.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad.mad_easyread;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,86 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class ProfileFragment extends Fragment {
+    FirebaseAuth mAuth;
+    EditText usernameEditText;
+    EditText emailEditText;
+    EditText passwordEditText;
+
+    Button updateBtn;
+    Button logoutBtn;
+
 
     public ProfileFragment(){
 
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        usernameEditText = view.findViewById(R.id.usernameEditText);
+        emailEditText = view.findViewById(R.id.emailEditText);
+        passwordEditText = view.findViewById(R.id.passwordEditText);
+
+        logoutBtn = view.findViewById(R.id.logout);
+        updateBtn = view.findViewById(R.id.updateBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String currentUserName = currentUser.getDisplayName();
+
+        usernameEditText.setText(currentUserName);
+        emailEditText.setText(currentUser.getEmail());
+        emailEditText.setEnabled(false);
+
+
+
+
+        updateBtn.setOnClickListener(v -> {
+
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(String.valueOf(usernameEditText.getText()))
+                    .build();
+
+            currentUser.updateProfile(profileUpdates).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getActivity(), "Update profile success!", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                    startActivity(getActivity().getIntent());
+                }
+            });
+        });
+
+        logoutBtn.setOnClickListener(v -> {
+            mAuth.signOut();
+            Intent welcomeIntent = new Intent(getActivity(), WelcomeActivity.class);
+            startActivity(welcomeIntent);
+            getActivity().finish();
+        });
+
+        return view;
+
+
+
     }
 }
