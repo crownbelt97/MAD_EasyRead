072d19301c5048f4c32b24a9f4af20d858f365ec
KahYengP <s10245147@connect.np.edu.sg>
2023-07-19 15:35:16 +0800

    Added searchview that is able to search via username and display it onto a listview

--- app/src/main/java/sg/edu/np/mad/easyread/AddFriendsActivity.java
+++ app/src/main/java/sg/edu/np/mad/easyread/AddFriendsActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,34 +23,54 @@ import sg.edu.np.mad.easyread.R;
 
 public class AddFriendsActivity extends AppCompatActivity {
 
+    private ArrayList<String> allUsernames;
+    private ArrayList<String> displayedUsernames;
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_add_friends);
+
+        ListView listView = findViewById(R.id.listView);
+        allUsernames = new ArrayList<>();
+        displayedUsernames = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, displayedUsernames);
+        listView.setAdapter(adapter);
+
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Not needed, but required to implement
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter the usernames based on the search query
+                filterUsernames(newText);
+                return true;
+            }
+        });
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<String> usernames = new ArrayList<>();
+                allUsernames.clear();
 
                 for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                     String usernameDB = String.valueOf(userSnapshot.child("username").getValue(String.class));
-                    usernames.add(usernameDB);
+                    allUsernames.add(usernameDB);
                 }
 
-                // Here, 'usernames' ArrayList contains all the usernames from the "users" node
-                // You can now use this ArrayList as needed
-                // For example, you can display them in a list, use it in an adapter, etc.
-
-                // Example: Displaying the usernames in the Logcat
-                for (String username : usernames) {
-                    Log.d("Usernames", username);
-                }
+                // Update the ListView with the new data
+                filterUsernames(""); // Initial filter to display an empty list
             }
 
             @Override
@@ -56,4 +79,17 @@ public class AddFriendsActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void filterUsernames(String query) {
+        displayedUsernames.clear();
+
+        for (String username : allUsernames) {
+            if (username.toLowerCase().contains(query.toLowerCase())) {
+                displayedUsernames.add(username);
+            }
+        }
+
+        adapter.notifyDataSetChanged();
+    }
 }
+
