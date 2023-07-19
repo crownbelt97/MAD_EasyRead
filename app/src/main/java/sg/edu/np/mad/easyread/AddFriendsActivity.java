8fdba5de02e522710af751ea68123c54fb6907b0
KahYengP <s10245147@connect.np.edu.sg>
2023-07-19 15:13:29 +0800

    Added usernames into arraylist

--- app/src/main/java/sg/edu/np/mad/easyread/AddFriendsActivity.java
+++ app/src/main/java/sg/edu/np/mad/easyread/AddFriendsActivity.java
@@ -1,10 +1,22 @@
 package sg.edu.np.mad.easyread;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import sg.edu.np.mad.easyread.R;
 
 public class AddFriendsActivity extends AppCompatActivity {
 
@@ -15,5 +27,33 @@ public class AddFriendsActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_add_friends);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<String> usernames = new ArrayList<>();
+
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    String usernameDB = String.valueOf(userSnapshot.child("username").getValue(String.class));
+                    usernames.add(usernameDB);
+                }
+
+                // Here, 'usernames' ArrayList contains all the usernames from the "users" node
+                // You can now use this ArrayList as needed
+                // For example, you can display them in a list, use it in an adapter, etc.
+
+                // Example: Displaying the usernames in the Logcat
+                for (String username : usernames) {
+                    Log.d("Usernames", username);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle database error if necessary
+            }
+        });
     }
 }
