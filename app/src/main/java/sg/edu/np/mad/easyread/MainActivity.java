5ec51826fa1cee45bc93f2f33933d72f647763b2
Mikhail Adam Zahadi <S10239784@connect.np.edu.sg>
2023-07-27 01:22:54 +0800

    Favourites in DetailsFragment working
    //TODO need to show on favouritesFragment

--- app/src/main/java/sg/edu/np/mad/easyread/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/easyread/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import sg.edu.np.mad.easyread.R;
